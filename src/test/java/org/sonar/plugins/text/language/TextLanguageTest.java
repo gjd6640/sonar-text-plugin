d8ccfc52e9c163ed7606288258f185578052f7cd
Gordon Daugherty <gordon@idisagree.org>
2021-08-25 13:29:19 -0500

    Resolved deprecated mockito class

--- src/test/java/org/sonar/plugins/text/language/TextLanguageTest.java
+++ src/test/java/org/sonar/plugins/text/language/TextLanguageTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.sonar.api.config.Configuration;
 import org.sonar.plugins.text.TextLanguage;
 import org.sonar.plugins.text.TextPlugin;
